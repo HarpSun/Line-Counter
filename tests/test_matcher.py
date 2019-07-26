5add5750f93a4dbde6c98cdfb450866a44caf54c
HarpSun <srcasdf@qq.com>
2019-07-26 14:13:15 +0800

    add unittest

---
+++ tests/test_matcher.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, main
+
+from line_counter import Matcher
+
+
+class TestMatcher(TestCase):
+    file = 'ignore.txt'
+    matcher = Matcher.init_by_file(file)
+
+    def test_init_by_file(self):
+        self.assertEqual(['ignoreA'], self.matcher._exclude_dirs)
+        self.assertEqual(['ignore.py'], self.matcher._exclude_files)
+        self.assertEqual(['py'], self.matcher._exclude_extensions)
+
+    def test_match_dir_by_name(self):
+        self.assertEqual(True, self.matcher.match_dir_by_name('ignoreA'))
+
+    def test_match_file_by_name(self):
+        self.assertEqual(True, self.matcher.match_file_by_name('ignore.py'))
+        self.assertEqual(True, self.matcher.match_file_by_name('test.py'))
+
+
+if __name__ == '__main__':
+    main()
